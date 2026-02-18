4ab99cdd1dfe63fe5a7d7212202a78a4d4725054
ClaudioPaulo71 <claudiopaulousa@gmail.com>
2026-02-18 11:02:26 -0600

    Fix: Support docker compose plugin

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,16 @@
 # Update code
 git pull origin main
 
+# Define docker compose command
+if command -v docker-compose &> /dev/null; then
+    DOCKER_COMPOSE="docker-compose"
+else
+    DOCKER_COMPOSE="docker compose"
+fi
+
 # Build and deploy with production config
 # We use -f to merge the base config with the prod override
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d --build --remove-orphans
+$DOCKER_COMPOSE -f docker-compose.yml -f docker-compose.prod.yml up -d --build --remove-orphans
 
 # Prune unused images to save space
 docker image prune -f
